518b3432cb729f749088bc8bdd9755a429a001c4
Dron <eptit@mail.ru>
2022-07-26 14:09:27 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int numbers(int n, int m)
+{
+    if (n == m) return n;
+    else return n + numbers(n + 1, m);
+}
+
+Console.WriteLine(numbers(1, 10));
